5c67eabb90a312bb4de32de9b7831fac799d30ba
RoyNwndk <76222626+RoyNwndk@users.noreply.github.com>
2024-12-22 17:58:48 +0100

    Initial Commit

---
+++ Finances/FinancesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinancesApp.swift
+//  Finances
+//
+//  Created by Roy Nieuwendijk on 22/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct FinancesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FinancesTests/FinancesTests.swift
@@ -0,0 +1,17 @@
+//
+//  FinancesTests.swift
+//  FinancesTests
+//
+//  Created by Roy Nieuwendijk on 22/12/2024.
+//
+
+import Testing
+@testable import Finances
+
+struct FinancesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
